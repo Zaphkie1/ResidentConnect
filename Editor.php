54d7924d6731c2a1bef976ff4b2b126eef475e7d
Edgar Trejo <89409775+Zaphkie1@users.noreply.github.com>
2024-03-26 19:59:53 -0600

    Cambios y carga de nuevos archivos

---
+++ Editor.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Empresa</title>
+    <link rel="stylesheet" href="http://localhost/ResidentConnect/assets/css/editorStyle.css">
+</head>
+
+<body>
+    <form action="php/Agregar.php" method="POST">
+        <h2>Agregar Empresa</h2>
+        <label for="empresa">Empresa:</label>
+        <input type="text" id="empresa" name="empresa" required>
+
+        <label for="carrera">Carrera:</label>
+        <input type="text" id="carrera" name="carrera" required>
+
+        <label for="puesto">Puesto:</label>
+        <input type="text" id="puesto" name="puesto" required>
+
+        <input type="submit" value="Agregar" name="Agregar">
+    </form>
+
+
+</body>
+
+</html>
